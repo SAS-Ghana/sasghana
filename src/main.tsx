8c12665b64a1e9599addeca020838299e22890c5
Dzornoboa <37811509+dzornoboa@users.noreply.github.com>
2026-07-27 18:03:11 +0000

    Load rebuilt employee portal styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { SasPeopleApp } from "../app/sas-people-app";
 import "../app/globals.css";
 import "../app/employee-shell.css";
+import "../app/employee-portal-v2.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
